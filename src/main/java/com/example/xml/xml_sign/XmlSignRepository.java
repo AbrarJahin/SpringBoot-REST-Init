8823a7ad9644b17766eb2270f585d404836eba5b
S. M. Abrar Jahin <abrarjahin@live.com>
2019-12-24 12:50:19 +0600

    New Controller Added

---
+++ src/main/java/com/example/xml/xml_sign/XmlSignRepository.java
@@ -0,0 +1,10 @@
+package com.example.xml.xml_sign;
+
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface XmlSignRepository extends CrudRepository<XmlSign, Integer> {
+
+}
