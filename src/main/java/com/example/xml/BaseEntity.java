c6456f57d2aa6799fb2b9d1f32165421e0dcd77c
S. M. Abrar Jahin <AbrarJahin@users.noreply.github.com>
2019-12-24 18:48:19 +0600

    Crud example (#1)
    * CRUD working, Base Model config neeed to be done
    
    * Search by specific field done

--- src/main/java/com/example/xml/BaseEntity.java
+++ src/main/java/com/example/xml/BaseEntity.java
@@ -21,7 +21,7 @@ public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)//GenerationType.IDENTITY
     @Basic(optional = false)
     @Column(name = "id", nullable = false, columnDefinition = "BIGINT UNSIGNED")
     protected Long id;
